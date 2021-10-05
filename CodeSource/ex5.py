62c25a415d1bc2647b29347800370556c2540d0b
Seisen99 <lpfrancisco99@gmail.com>
2021-10-06 01:20:11 +0200

    LearningPython

---
+++ CodeSource/ex5.py
@@ -0,0 +1,19 @@
+name = 'Zed A. Shaw'
+age = 35 # not a lie
+height = 74 # inches
+weight = 180 # lbs
+eyes = 'Blue'
+teeth = 'White'
+hair = 'Brown'
+height_in_cm = height * 2.54
+weight_in_kg = weight * 0.45
+
+print(f"Let's talk about {name}.")
+print(f"He's {height} inches or {height_in_cm} cm tall.")
+print(f"He's {weight} pounds or {weight_in_kg} kgs heavy.")
+print("Actually that's not too heavy.")
+print(f"He's got {eyes} eyes and {hair} hair.")
+print(f"His teeth are usually {teeth} depending on the coffee.")
+
+total = age + height + weight
+print(f"If I add {age}, {height}, and {weight} I get {total}.")
