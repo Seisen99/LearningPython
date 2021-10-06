428feb7884dcf7ac993b6896ae8156653fe03615
Seisen99 <lpfrancisco99@gmail.com>
2021-10-06 02:40:25 +0200

    LearningPython

---
+++ CodeSource/EscapeSequences.py
@@ -0,0 +1,11 @@
+Backslash = "\\ ça affiche un \\ avec 2 \\" #ça affiche 1 backslash \ avec 2 backslash
+Single_quote = "\' ça affiche une apostrophe" #ça affiche une apostrophe
+ASCII_bell = "\a exemple" #ça affiche ASCII bell ?
+
+
+
+
+
+print(Backslash)
+print(Single_quote)
+print(ASCII_bell)
